66a86bf0f2c7cfba21b62def11349d38a5da7351
Andrey Brovko <brov.ko@ya.ru>
2021-11-03 11:19:11 +0300

    Help heading string interpolation.

--- GherkinSyncTool/CommandLineOptions/CommandLineParserHelper.cs
+++ GherkinSyncTool/CommandLineOptions/CommandLineParserHelper.cs
@@ -11,25 +11,25 @@ namespace GherkinSyncTool.CommandLineOptions
     public static class CommandLineParserHelper
     {
         private static readonly Logger Log = LogManager.GetLogger(MethodBase.GetCurrentMethod()?.DeclaringType?.Name);
-        private static readonly string Heading = $"{Program.Version}Quantori GherkinSyncTool is an open-source console " +
-                                                  $"application that synchronizes tests scenarios in Gherkin syntax (also known as feature files) " +
-                                                  $"with a test management system or any other destination. " +
-                                                  $"https://github.com/quantori/GherkinSyncTool";
-        
+
+        private static readonly string Heading = @$"{Program.Version}Quantori GherkinSyncTool is an open-source console 
+                                application that synchronizes tests scenarios in Gherkin syntax (also known as feature files) 
+                                with a test management system or any other destination. https://github.com/quantori/GherkinSyncTool";
+
         public static Type[] LoadCliVerbsTypes()
         {
             return Assembly.GetExecutingAssembly().GetTypes()
                 .Where(t => t.GetCustomAttribute<VerbAttribute>() != null).ToArray();
         }
 
-        public static void DisplayHelp(ParserResult<object> parserResult, IEnumerable<Error> errs) 
+        public static void DisplayHelp(ParserResult<object> parserResult, IEnumerable<Error> errs)
         {
             if (errs.IsVersion())
             {
                 Log.Info(Program.Version);
                 Environment.Exit(1);
             }
-            
+
             Log.Info(HelpText.AutoBuild(parserResult, h =>
             {
                 h.AutoVersion = false;
