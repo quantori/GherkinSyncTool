40d269772fbbf0aca49e18da1d0a5ec230a281b1
Brrovko <brov.ko@ya.ru>
2021-11-03 12:02:32 +0300

    Customize help. Sync options: testrail, azuredevops. (#13)
    * Customize help
    * Add sync options: testrail, azuredevops
    * README.md updated

---
+++ GherkinSyncTool/CommandLineOptions/CommandLineParserHelper.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using CommandLine;
+using CommandLine.Text;
+using NLog;
+
+namespace GherkinSyncTool.CommandLineOptions
+{
+    public static class CommandLineParserHelper
+    {
+        private static readonly Logger Log = LogManager.GetLogger(MethodBase.GetCurrentMethod()?.DeclaringType?.Name);
+
+        private static readonly string Heading = @$"{Program.Version}Quantori GherkinSyncTool is an open-source console 
+                                application that synchronizes tests scenarios in Gherkin syntax (also known as feature files) 
+                                with a test management system or any other destination. https://github.com/quantori/GherkinSyncTool";
+
+        public static Type[] LoadCliVerbsTypes()
+        {
+            return Assembly.GetExecutingAssembly().GetTypes()
+                .Where(t => t.GetCustomAttribute<VerbAttribute>() != null).ToArray();
+        }
+
+        public static void DisplayHelp(ParserResult<object> parserResult, IEnumerable<Error> errs)
+        {
+            if (errs.IsVersion())
+            {
+                Log.Info(Program.Version);
+                Environment.Exit(1);
+            }
+
+            Log.Info(HelpText.AutoBuild(parserResult, h =>
+            {
+                h.AutoVersion = false;
+                h.AdditionalNewLineAfterOption = false;
+                h.Heading = Heading;
+                h.AddEnumValuesToHelpText = true;
+                return h;
+            }));
+            Environment.Exit(1);
+        }
+    }
+}
---
+++ GherkinSyncTool/CommandLineOptions/SyncOptions.cs
@@ -0,0 +1,14 @@
+﻿using CommandLine;
+
+namespace GherkinSyncTool.CommandLineOptions
+{
+    [Verb("sync", true, HelpText = "Start synchronization with Test Management System.")]
+    class SyncOptions
+    {
+        [Option("testrail", HelpText = "Synchronize with TestRail")]
+        public bool Testrail { get; set; }
+
+        [Option("azuredevops", HelpText = "Synchronize with Azure DevOps")]
+        public bool AzureDevOps { get; set; }
+    }
+}
