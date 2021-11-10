ff810e9391676923e177f6ce88779ec50969bc9c
Andrey Brovko <brov.ko@ya.ru>
2021-11-10 12:01:21 +0300

    - Add background keyword - Add test parameters

--- GherkinSyncTool.Synchronizers.AzureDevOps/Content/CaseContentBuilder.cs
+++ GherkinSyncTool.Synchronizers.AzureDevOps/Content/CaseContentBuilder.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Xml.Serialization;
 using Gherkin.Ast;
 using GherkinSyncTool.Models;
 using GherkinSyncTool.Models.Utils;
@@ -9,6 +12,7 @@ using GherkinSyncTool.Synchronizers.AzureDevOps.Model;
 using Microsoft.TeamFoundation.TestManagement.WebApi;
 using Microsoft.VisualStudio.Services.WebApi.Patch;
 using Microsoft.VisualStudio.Services.WebApi.Patch.Json;
+using DataTable = Gherkin.Ast.DataTable;
 
 namespace GherkinSyncTool.Synchronizers.AzureDevOps.Content
 {
@@ -45,12 +49,42 @@ namespace GherkinSyncTool.Synchronizers.AzureDevOps.Content
                 }
             };
 
-            AddTestStepsToJsonDocument(patchDocument, GetStepsFromFeatureFile(scenario, featureFile));
+            AddTestStepsToJsonDocument(patchDocument, scenario, featureFile);
+            AddTestTagsToJsonDocument(patchDocument, scenario, featureFile);
+            AddParametersToJsonDocument(patchDocument, scenario);
+
+            return patchDocument;
+        }
+
+        private void AddParametersToJsonDocument(JsonPatchDocument patchDocument, Scenario scenario)
+        {
+            if(!scenario.Examples.Any()) return;
             
+            var patchDocumentParameters = new JsonPatchDocument
+            {
+                new()
+                {
+                    Operation = Operation.Add,
+                    Path = $"/fields/{WorkItemFields.Parameters}",
+                    Value = GetXmlTestParameters(scenario)
+                },
+                new()
+                {
+                    Operation = Operation.Add,
+                    Path = $"/fields/{WorkItemFields.LocalDataSource}",
+                    Value = GetXmlTestParametersValues(scenario)
+                }
+            };
+            
+            patchDocument.AddRange(patchDocumentParameters);
+        }
+
+        private void AddTestTagsToJsonDocument(JsonPatchDocument patchDocument, Scenario scenario, IFeatureFile featureFile)
+        {
             var tags = ConvertToStringTags(scenario, featureFile);
             if (!string.IsNullOrWhiteSpace(tags))
             {
-                var tagsOperation = new JsonPatchOperation()
+                var tagsOperation = new JsonPatchOperation
                 {
                     Operation = Operation.Add,
                     Path = $"/fields/{WorkItemFields.Tags}",
@@ -58,56 +92,64 @@ namespace GherkinSyncTool.Synchronizers.AzureDevOps.Content
                 };
                 patchDocument.Add(tagsOperation);
             }
-            
-            return patchDocument;
         }
 
-        private JsonPatchDocument AddTestStepsToJsonDocument(JsonPatchDocument jsonPatchDocument, List<string> steps)
+        private void AddTestStepsToJsonDocument(JsonPatchDocument jsonPatchDocument, Scenario scenario, IFeatureFile featureFile)
         {
+            var steps = GetStepsFromFeatureFile(scenario, featureFile);
             var testBase = _testBaseHelper.Create();
             foreach (var step in steps)
             {
-                ITestStep testStep = testBase.CreateTestStep();
+                var testStep = testBase.CreateTestStep();
                 testStep.Title = step;
                 testBase.Actions.Add(testStep);    
             }
             
-            return testBase.SaveActions(jsonPatchDocument);
+            testBase.SaveActions(jsonPatchDocument);
         }
 
         private List<string> GetStepsFromFeatureFile(Scenario scenario, IFeatureFile featureFile)
         {
-            var scenarioSteps = ExtractStepsFromScenario(scenario.Steps.ToList());
+            var testParameters = GetTestParameters(scenario);
+            
+            var scenarioSteps = ExtractStepsFromScenario(scenario.Steps.ToList(), testParameters);
 
             var background = featureFile.Document.Feature.Children.OfType<Background>().FirstOrDefault();
             if (background is not null)
             {
-                var backgroundSteps = ExtractStepsFromScenario(background.Steps.ToList());
-                //TODO:  backgroundSteps.ForEach(s => s = $"Background: {s}");
+                var backgroundSteps = ExtractStepsFromScenario(background.Steps.ToList(), testParameters);
+
+                for (var index = 0; index < backgroundSteps.Count; index++)
+                {
+                    backgroundSteps[index] = $"Background: {backgroundSteps[index]}";
+                }
+
                 return backgroundSteps.Concat(scenarioSteps).ToList();
             }
 
             return scenarioSteps;
         }
         
-        private List<string> ExtractStepsFromScenario(List<Step> steps)
+        private List<string> ExtractStepsFromScenario(List<Step> steps, TestParameters testParameters)
         {
             List<string> resultSteps = new List<string>();
             foreach (var step in steps)
             {
                 var keywordFormatted = $"<span style=\"color:RoyalBlue\">{step.Keyword.Trim()} </span>";
-                var stepFormatted = step.Text.Replace("<","<span style=\"color:PowderBlue\">&lt").Replace(">","&gt</span>");
+
+                var stepFormatted = FormatTestParameters(testParameters, step.Text);
                 
                 var fullStep = keywordFormatted + stepFormatted;
                 
                 if (step.Argument is DocString docString)
                 {
-                    fullStep += Environment.NewLine + docString.Content;
+                    var stepArgument = FormatTestParameters(testParameters, docString.Content);
+                    fullStep += Environment.NewLine + stepArgument;
                 }
                 
                 if (step.Argument is DataTable table)
                 {
-                    fullStep += Environment.NewLine + BuildTable(table.Rows.ToList());
+                    fullStep += Environment.NewLine + BuildTable(table.Rows.ToList(), testParameters);
                 }
 
                 resultSteps.Add(fullStep);
@@ -115,8 +157,22 @@ namespace GherkinSyncTool.Synchronizers.AzureDevOps.Content
 
             return resultSteps;
         }
-        
-        private string BuildTable(List<TableRow> tableRows)
+
+        private static string FormatTestParameters(TestParameters testParameters, string stringWithParameters)
+        {
+            //Azure DevOps test parameters don't allow white spaces.
+            foreach (var param in testParameters.Param)
+            {
+                stringWithParameters =
+                    stringWithParameters.Replace($"<{param.Name}>", $"<{param.Name.Replace(" ", string.Empty)}>");
+            }
+            
+            stringWithParameters = stringWithParameters.Replace("<","<span style=\"color:LightSeaGreen\">@").Replace(">","</span>");
+
+            return stringWithParameters;
+        }
+
+        private string BuildTable(List<TableRow> tableRows, TestParameters testParameters)
         {
             var table = new StringBuilder();
             table.Append("<table style=\"width: 100%;\">");
@@ -137,7 +193,7 @@ namespace GherkinSyncTool.Synchronizers.AzureDevOps.Content
                 var row = tableRows[i];
                 foreach (var cell in row.Cells)
                 {
-                    table.Append($"<td style=\"border: 1px solid RoyalBlue; \">{cell.Value}</td>");
+                    table.Append($"<td style=\"border: 1px solid RoyalBlue; \">{FormatTestParameters(testParameters, cell.Value)}</td>");
                 }
 
                 table.Append("</tr>");
@@ -157,23 +213,78 @@ namespace GherkinSyncTool.Synchronizers.AzureDevOps.Content
             
             return preconditions.ToString();
         }
+        
+        private string GetXmlTestParameters(Scenario scenario)
+        {
+            var testParameters = GetTestParameters(scenario);
+            
+            //Azure DevOps test parameters don't allow white spaces.
+            for (var index = 0; index < testParameters.Param.Count; index++)
+            {
+                testParameters.Param[index].Name = testParameters.Param[index].Name.Replace(" ", string.Empty);
+            }
 
-        //TODO:
-        // private string GetParametersExamples(Scenario scenario)
-        // {
-        //     var examples = scenario.Examples.ToList();
-        //     if (examples.Any())
-        //     {
-        //         foreach (var example in examples)
-        //         {
-        //             preconditions.Append($"## {example.Keyword}: {example.Name}");
-        //
-        //             var tableRows = new List<TableRow> {example.TableHeader};
-        //             tableRows.AddRange(example.TableBody);
-        //             preconditions.Append(ConvertToStringTable(tableRows));
-        //         }
-        //     }
-        // }
+            var xmlSerializer = new XmlSerializer(typeof(TestParameters));
+
+            using TextWriter stringWriter = new StringWriter();
+            xmlSerializer.Serialize(stringWriter, testParameters);
+            return stringWriter.ToString();
+        }
+        
+        private TestParameters GetTestParameters(Scenario scenario)
+        {
+            var examples = scenario.Examples.ToList();
+            if (!examples.Any()) throw new ArgumentNullException(nameof(scenario));
+
+            var testParameters = new TestParameters {Param = new List<Param>()};
+            
+            foreach (var headerCell in examples.First().TableHeader.Cells)
+            {
+                testParameters.Param.Add(new Param{Name = headerCell.Value});
+            }
+            
+            return testParameters;
+        }
+        
+        private string GetXmlTestParametersValues(Scenario scenario)
+        {
+            var examples = scenario.Examples.ToList();
+            if (!examples.Any()) throw new ArgumentNullException(nameof(scenario));
+            
+            var xmlSerializer = new XmlSerializer(typeof(DataSet));
+            var dataSet = new DataSet("NewDataSet");
+            var dataTable = new System.Data.DataTable("Table1");
+            
+            var testParameters = GetTestParameters(scenario);
+            
+            foreach (var param in testParameters.Param)
+            {
+                //Azure DevOps test parameters don't allow white spaces.
+                var dataColumn = new DataColumn(param.Name.Replace(" ", string.Empty));
+                dataTable.Columns.Add(dataColumn);
+            }
+            dataSet.Tables.Add(dataTable);
+
+            foreach (var example in examples)
+            {
+                foreach (var tableRow in example.TableBody)
+                {
+                    var dataRow = dataTable.NewRow();
+                    var tableCells = tableRow.Cells.ToList();
+                    for (int i = 0; i < tableCells.Count; i++)
+                    {
+                        dataRow[i] = tableCells[i].Value;
+                    }
+
+                    dataTable.Rows.Add(dataRow);
+                }
+            }
+
+            using TextWriter textWriter = new StringWriter();
+            xmlSerializer.Serialize(textWriter, dataSet);
+        
+            return textWriter.ToString();
+        }
         
         private string ConvertToStringTags(Scenario scenario, IFeatureFile featureFile)
         {
