1a00d276717140f3fb89a137710e6f6e04b85ee8
Andrey Brovko <brov.ko@ya.ru>
2021-11-24 09:58:08 +0300

    Add HtmlTagIds class

---
+++ GherkinSyncTool.Synchronizers.AzureDevOps/Model/HtmlTagIds.cs
@@ -0,0 +1,10 @@
+﻿namespace GherkinSyncTool.Synchronizers.AzureDevOps.Model
+{
+    public static class HtmlTagIds
+    {
+        /// <summary>
+        /// Uses for identifying an HTML element which stored feature file path.
+        /// </summary>
+        public const string FeatureFilePathId = "FeatureFilePath";
+    }
+}
